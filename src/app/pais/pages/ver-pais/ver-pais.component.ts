f4bad21093aaf204ea66d3cbfad8082068eb78ab
FelipeEspinoza <felipe.al377@gmail.com>
2022-10-19 23:58:40 -0300

    feat: fin de la pantalla ver país

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators'
+import { switchMap, tap } from 'rxjs/operators'
+import { Country } from '../../interfaces/pais.interfaces';
 
 import { PaisService } from '../../services/pais.service';
 
@@ -12,6 +13,8 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country;
+
   constructor(
      private activatedRoute: ActivatedRoute, 
      private paisService: PaisService
@@ -21,11 +24,11 @@ export class VerPaisComponent implements OnInit {
 
     this.activatedRoute.params
       .pipe(
-        switchMap(  ({id}) => this.paisService.getPaisPorAlpha( id ))
+        switchMap(  ({id}) => this.paisService.getPaisPorAlpha( id )),
+        tap( resp => console.log(resp)
+         )
       )
-      .subscribe( resp => {
-        console.log(resp);
-      })
+      .subscribe( pais => this.pais = pais );
 
     // this.activatedRoute.params
     //   .subscribe( ({ id }) => { //aplicamos destructuracion a params para sacar el id
